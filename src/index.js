4485d59ab30a46e00fa54cbcde3bfc95c08aa494
kv9991 <exide-pra@yandex.ru>
2017-08-04 16:10:07 +0300

    Remove non-op deps

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 // important
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './App.js'
-import { createBrowserHistory } from 'history'
 
 ReactDOM.render(
   <HashRouter>
