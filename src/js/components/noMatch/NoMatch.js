d9ca4640bf6c7ce8772597b08e0b3915c6127d9e
kv9991 <exide-pra@yandex.ru>
2017-08-28 14:31:43 +0300

    add components folder again

---
+++ src/js/components/noMatch/NoMatch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NoMatch.css';
+
+export default class NoMatch extends React.Component {
+  render() {
+    return (
+      <div className="Container NoMatch fullpage">
+      	<h1>Oops..</h1>
+      	<p className="primary">404. Возможно, этой страницы больше не существует или никогда не существовала</p>
+      	<Link to=""><button className="button">На главную</button></Link>
+      </div>
+    );
+  }
+}
